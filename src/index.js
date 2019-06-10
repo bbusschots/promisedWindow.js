99399c78bd20180a51c362e7dec81a1c14923e90
Bart Busschots <opensource@bartificer.net>
2019-06-10 17:51:08 +0100

    Initial Working Version

--- src/index.js
+++ src/index.js
@@ -1,34 +1,93 @@
+import is from 'is_js';
 
 /**
  * A Promise wrapper around `window.confirm()`.
  *
- * @param {String} [messaage='Are you sure?'] The message to include within the alert dialogue.
- * @param {*} [resolveTo=true] The value the conirmation promise should resolve to.
- * @param {boolean} [rejectOnCancel=true] Reject the promise if the user does not confirm, if set to `false` the promise resolves as `false` when the user cancels the confirmation.
- * @param {String} [rejectErrorMessage='Confirmation Refused'] The error message to reject the Promise with if the user does not confirm. 
- * @returns {Promise} Resturns a Promise that resovlves when the user accepts the confirmation, and rejects when they do not.
+ * @param {String} [messaage='Are you sure?'] The message to include within the confirmation dialogue.
+ * @param {object} [options={}]
+ * @param {*} [options.confirmValue=true] The value the promise should resolve to when the user confirms.
+ * @param {*} [options.declineValue=false] The value the promise should resolve to when the user declines.
+ * @param {boolean} [options.rejectOnDecline=true] Reject the promise if the user does not confirm, if set to `false` the promise resolves as `options.declineValue` when the user cancels the confirmation.
+ * @param {String} [options.declineErrorMessage='Confirmation Refused'] The error message to reject the Promise with if the user does not confirm. 
+ * @returns {Promise} Resturns a Promise that resolves or rejects when the user accepts or declines the confirmation.
  */
-function confirm(message='Are you sure?', resolveTo=true, rejectOnCancel=true, rejectErrorMessage='Confirmation Refused'){
+function confirm(message='Are you sure?', options={}){
     // force all the types to be as expected
-    message = String(message);
-    rejectOnCancel = rejectOnCancel ? true : false;
-    rejectErrorMessage = String(rejectErrorMessage);
+    message = is.string(message) ? message : String(message);
+    options = is.object(options) ? options : {};
+    options.confirmValue = is.undefined(options.confirmValue) ? true : options.confirmValue;
+    options.declineValue = is.undefined(options.declineValue) ? false : options.declineValue;
+    if(is.undefined(options.rejectOnDecline)) options.rejectOnDecline = true;
+    options.rejectOnDecline = options.rejectOnDecline ? true : false;
+    if(is.undefined(options.declineErrorMessage)) options.declineErrorMessage = 'Confirmation Refused';
+    options.declineErrorMessage = is.string(options.declineErrorMessage) ? options.declineErrorMessage : String(options.declineErrorMessage);
     
-    // build the Promise to return
+    // build the Promise and return it
     return new Promise(function (resolve) {
         // get confirmation and react appropriately
         if(window.confirm(message)){
-            return resolve(resolveTo);
+            return resolve(options.confirmValue);
         }
         
         // not confirmed
-        if(rejectOnCancel){
-            throw new Error(rejectErrorMessage);
+        if(options.rejectOnDecline){
+            throw new Error(options.declineErrorMessage);
         }
-        resolve(false);
+        resolve(options.declineValue);
+    });
+}
+
+/**
+ * A Promise wrapper around `window.alert()`.
+ *
+ * @param {String} [messaage='Something happened!'] The message to include within the alert dialogue.
+ * @param {object} [options={}]
+ * @param {*} [options.resolveValue=true] The value the promise should resolve to when the user OKs the alert.
+ * @returns {Promise} Resturns a Promise that resolves when the user OKs the alert.
+ */
+function alert(message='Something happened!?', options={}){
+    // force all the types to be as expected
+    message = is.string(message) ? message : String(message);
+    options = is.object(options) ? options : {};
+    options.resolveValue = is.undefined(options.resolveValue) ? true : options.resolveValue;
+    
+    // build the Promise and return it
+    return new Promise(function (resolve) {
+        // show the alert then resolve
+        window.alert(message);
+        resolve(options.resolveValue);
+    });
+}
+
+/**
+ * A Promise wrapper around `window.setTimeout()`.
+ *
+ * @param {function} delayedCallback - The function to execute when the timeout finishes.
+ * @param {number} [delay=3000] - The delay before executing `delayedCallback` in milliseconds.
+ * @param {Array} [callbackArgs=[]] - The argumenmts to pass when executing `delayedCallback`.
+ * @param {object} [options={}]
+ * @param {*} [options.resolveValue=true] The value the promise should resolve to when the user OKs the alert.
+ * @returns {Promise} Resturns a Promise that resolves or rejects when the user accepts or declines the confirmation.
+ */
+function setTimeout(delayedCallback, delay=3000, callbackArgs=[], options={}){
+    // Validate the callback & delay
+    if(is.not.function(delayedCallback)) throw new TypeError('First parameter must be a callback');
+    if(!String(delay).match(/^\d+$/)) throw new TypeError('Second parameter must be an integer');
+    
+    // force all the other types to be as expected
+    callbackArgs = is.array(callbackArgs) ? callbackArgs : [];
+    options = is.object(options) ? options : {};
+    options.resolveValue = is.undefined(options.resolveValue) ? true : options.resolveValue;
+    
+    // build the Promise and return it
+    return new Promise(function (resolve) {
+        window.setTimeout(function(){
+            delayedCallback(...callbackArgs);
+            resolve(options.resolveValue);
+        }, delay);
     });
 }
 
 export default {
-    confirm
+    confirm, alert, setTimeout, timeout: setTimeout
 };
